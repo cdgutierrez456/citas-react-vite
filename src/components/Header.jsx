57808c40f87873cdaa406a4b7a7bcc47e219dd6b
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2023-03-19 20:46:34 -0500

    Aplicando estilos iniciales al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 export default function Header() {
   return (
     <>
-      <header>
-        <h1>Desde header</h1>
-      </header>
+      <h1 className="font-black text-5xl text-center md:w-1/2 mx-auto">
+        Seguimiento Pacientes{" "}
+        <span className="text-indigo-600">Veterinaria</span>
+      </h1>
     </>
   );
 }
