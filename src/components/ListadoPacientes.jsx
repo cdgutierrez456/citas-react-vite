182853fdebcdce1c87d7c499f50a1bf894125108
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2023-04-08 19:42:28 -0500

    Mostrando secciones de aviso condicionales a si hay o no pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,17 +3,33 @@ import Paciente from "./Paciente";
 const ListadoPacientes = ({ patients }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold text-xl">
-          Pacientes y citas
-        </span>
-      </p>
+      {patients && patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            Listado de pacientes
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold text-xl">
+              Pacientes y citas
+            </span>
+          </p>
 
-      {patients.map((patient) => (
-        <Paciente key={patient.id} patient={patient} />
-      ))}
+          {patients.map((patient) => (
+            <Paciente key={patient.id} patient={patient} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando tus pacientes {""}
+            <span className="text-indigo-600 font-bold text-xl">
+              y apareceran en este lugar
+            </span>
+          </p>
+        </>
+      )}
     </div>
   );
 };
