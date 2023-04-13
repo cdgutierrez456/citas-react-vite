badcde6d7a24ac98f07ba8ec536448bb68e607d1
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2023-04-12 22:05:57 -0500

    Agregando almacenamiento en localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import Form from "./components/Form";
@@ -7,6 +7,18 @@ import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   const [patients, setPatients] = useState([]);
   const [patient, setPatient] = useState({});
+
+  useEffect(() => {
+    const getLS = () => {
+      const patientsLS = JSON.parse(localStorage.getItem("patients")) ?? [];
+      setPatients(patientsLS);
+    };
+    getLS();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("patients", JSON.stringify(patients));
+  }, [patients]);
 
   const deletePatient = (id) => {
     const updatedPatients = patients.filter((pat) => pat.id != id);
