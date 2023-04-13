6095c8ce63c99633d71bb032695041a021bb7816
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2023-04-12 22:30:12 -0500

    integracion continua

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(props) {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-1/2 mx-auto">
-        Seguimiento Pacientes{" "}
+        Seguimiento Pacientes Con GitHub {""}
         <span className="text-indigo-600">Veterinaria</span>
       </h1>
     </>
